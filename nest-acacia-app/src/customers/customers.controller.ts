24db2ce95d938e556332d81d1973bda1ec3dcb11
SnowvyTH <supanat617@gmail.com>
2026-02-08 23:00:18 +0700

    Day 1.3

--- nest-acacia-app/src/customers/customers.controller.ts
+++ nest-acacia-app/src/customers/customers.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Version } from '@nestjs/common';
 
-@Controller('customers')
+@Controller({
+    version: '1',
+    path: 'customers',
+})
+
 export class CustomersController {
 
+    // localhost:port/v1/customers
     @Get()
+    @HttpCode(200)
     findALl() {
         return 'findAll Customers'
+    }
+
+    // localhost:port/v2/customers
+    @Version('2')
+    @Get()
+    @HttpCode(200)
+    findAllV2() {
+        return 'findAll Customers v2'
     }
 
 }
