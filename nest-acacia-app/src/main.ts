8fa3a2096300d3be9dba4310de1e4280da270755
SnowvyTH <supanat617@gmail.com>
2026-02-10 23:09:18 +0700

    Day 2.1

--- nest-acacia-app/src/main.ts
+++ nest-acacia-app/src/main.ts
@@ -4,7 +4,7 @@ import { Version, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX ?? 'api');
   app.enableVersioning({
     type: VersioningType.URI,
   })
